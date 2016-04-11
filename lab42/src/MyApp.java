97396e4ba55fd6a8ec11672083b254d20159c79e
rcht08 <rcht08@gmail.com>
2016-04-11 17:49:40 +0300

    changebalance added

--- lab42/src/MyApp.java
+++ lab42/src/MyApp.java
@@ -3,8 +3,10 @@
 public class MyApp {
  public static void main(String[] args) {
  Product product = new Product("Edam", 3.3, 120);
+ product.changeBalance(20);
  System.out.println("Product value is " + product.countValue());
  product.printProduct();
+ 
  }
 }
 class Product {
@@ -17,10 +19,18 @@ this.price = price;
 this.amount = amount;
 }
 public double countValue() {
-return(amount * price);
+double result;
+result = amount * price;
+return result;
 }
 public void printProduct() {
 System.out.printf("Product %s, price %4.1f and balance %d pcs",
 name,price,amount);
 }
+public int changeBalance(int newvalue){
+    newvalue = this.amount;
+    return newvalue;
+    
+}
+
 }
